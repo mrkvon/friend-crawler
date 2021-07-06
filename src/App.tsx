3bb63d07898a2319ab44620803d82862a5ae097c
mrkvon <mrkvon@protonmail.com>
2021-07-06 05:34:50 +0200

    add bulma for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 function App() {
-  return <div>app</div>
+  return (
+    <p>
+      This is a basic app that can use{' '}
+      <a href="https://bulma.io/" title="The Bulma CSS framework">
+        Bulma
+      </a>{' '}
+      for styling.
+      <button className="button">hello</button>
+    </p>
+  )
 }
 
 export default App
