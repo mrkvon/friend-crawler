befdfb9fbd9a645164e3f89a98b8d62af8bba292
mrkvon <mrkvon@protonmail.com>
2021-07-09 07:38:28 +0200

    Add hook to fetch and update data
    Heavily copied from https://gitlab.com/vincenttunru/penny/-/commit/da2b1a5a70b49fe4bc1e29c453a26dcf3c6ed3db

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { SessionPrompt } from './components/SessionPrompt'
 import { useSessionInfo } from './hooks/sessionInfo'
+import { getStringNoLocale } from '@inrupt/solid-client'
+import { foaf } from 'rdf-namespaces'
+import { useProfile } from './hooks/profile'
 
 function App() {
   const info = useSessionInfo()
+  const profile = useProfile()
+
+  const name = profile
+    ? getStringNoLocale(profile.data, foaf.name)
+    : info?.webId
 
   return (
     <>
-      <SessionPrompt>Welcome {info?.webId}</SessionPrompt>
+      <SessionPrompt>Welcome {name}</SessionPrompt>
 
       <pre>{'There will be a content.'}</pre>
     </>
---
+++ src/hooks/profile.ts
@@ -0,0 +1,32 @@
+import { getThing, setThing, Thing } from '@inrupt/solid-client'
+import { useCallback } from 'react'
+import { useSessionInfo } from './sessionInfo'
+import { useDataset } from './dataset'
+
+export function useProfile() {
+  const sessionInfo = useSessionInfo()
+  const profileDoc = useDataset(sessionInfo?.webId ?? null)
+
+  const update = useCallback(
+    (profile: Thing) => {
+      if (!profileDoc?.data) return
+
+      const updatedProfileDoc = setThing(profileDoc.data, profile)
+      profileDoc.save(updatedProfileDoc)
+    },
+    [profileDoc],
+  )
+
+  if (
+    profileDoc === null ||
+    typeof profileDoc.data === 'undefined' ||
+    typeof sessionInfo?.webId === 'undefined'
+  )
+    return null
+
+  const profile = getThing(profileDoc.data, sessionInfo.webId)
+
+  if (profile === null) return null
+
+  return { data: profile, save: update }
+}
