b93c0b12fb2c98fe1363bd9c02a38f61c7f53f5a
mrkvon <mrkvon@users.noreply.github.com>
2021-08-06 11:44:45 +0000

    deploy: 1e816ff5e7748d79636acde6128878c389006de6

--- static/js/4.1ed5a21a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpfriend-crawler"]=this["webpackJsonpfriend-crawler"]||[]).push([[4],{215:function(n,c){},217:function(n,c){}}]);
-//# sourceMappingURL=4.1ed5a21a.chunk.js.map
---
+++ static/js/4.9935e3ab.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpfriend-crawler"]=this["webpackJsonpfriend-crawler"]||[]).push([[4],{217:function(n,c){},219:function(n,c){}}]);
+//# sourceMappingURL=4.9935e3ab.chunk.js.map
